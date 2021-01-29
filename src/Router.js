464b2384e77841f1c8e77128f672fac123d82170
Fachri <fachrighiffary@gmail.com>
2021-01-29 18:55:01 +0700

    first commit

---
+++ src/Router.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import 'react-native-gesture-handler'
+import {NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import Splash from './screens/splash';
+import Login from './screens/auth/login';
+
+
+const Stack = createStackNavigator();
+
+const Router = () => {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen 
+                    name='Splash' 
+                    component={Splash}
+                    options={{
+                    headerShown: false,
+                }}/>
+                <Stack.Screen 
+                    name='Login' 
+                    component={Login}
+                    options={{
+                    headerShown: false,
+                }}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Router
