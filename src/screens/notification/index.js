9fb60ced3067433794571fc3426546891186204d
hendrasolih <hendra.solih.jp@gmail.com>
2021-02-04 05:30:47 +0700

    notification

--- src/screens/notification/index.js
+++ src/screens/notification/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {TouchableOpacity, ScrollView} from 'react-native-gesture-handler';
 import Iconn from 'react-native-vector-icons/MaterialCommunityIcons';
 import Icon from 'react-native-vector-icons/AntDesign';
 import style from './notifStyle';
 
+import {useSelector} from 'react-redux';
+import {API_URL} from '@env';
+import axios from 'axios';
+
+const toPrice = (x) => {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+};
+
 const Notification = ({navigation}) => {
+  useEffect(() => {
+    getData();
+    return () => {
+      getData();
+    };
+  }, [history]);
+  const token_user = useSelector((state) => state.auth.token);
+  const [history, setHistory] = useState([]);
+
+  const getData = () => {
+    const config = {
+      headers: {
+        'x-access-token': 'Bearer ' + token_user,
+      },
+    };
+    axios
+      .get(API_URL + '/transaction/getAllInvoice', config)
+      .then((res) => {
+        setHistory(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <View style={{...style.mainContainer}}>
       <View style={{...style.header, marginLeft: 8}}>
@@ -22,67 +54,29 @@ const Notification = ({navigation}) => {
       <ScrollView>
         <View style={{...style.textContainer}}>
           <Text style={{...style.textHeader}}>Today</Text>
-          <View>
-            <TouchableOpacity style={{...style.allContacts}}>
-              <Iconn name="arrow-down" style={{...style.icon}} />
-              <View style={{marginHorizontal: 15}}>
-                <Text style={{...style.textContacts}}>
-                  Transfered from Joshua Lee
-                </Text>
-                <Text style={{...style.textPhone}}>Rp.220.000</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <View>
-            <TouchableOpacity style={{...style.allContacts}}>
-              <Iconn name="arrow-up" style={{...style.icona}} />
-              <View style={{marginHorizontal: 15}}>
-                <Text style={{...style.textContacts}}>
-                  Netflix Subscription
-                </Text>
-                <Text style={{...style.textPhone}}>Rp.149.000</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-   
-   
-        <View style={{...style.textContainer}}>
-          <Text style={{...style.textHeader}}>This Week</Text>
-          <View>
-            <TouchableOpacity style={{...style.allContacts}}>
-              <Iconn name="arrow-up" style={{...style.icona}} />
-              <View style={{marginHorizontal: 15}}>
-                <Text style={{...style.textContacts}}>
-                  Transfered to Jesica Lee
-                </Text>
-                <Text style={{...style.textPhone}}>Rp.100.000</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <View>
-            <TouchableOpacity style={{...style.allContacts}}>
-              <Iconn name="arrow-down" style={{...style.icon}} />
-              <View style={{marginHorizontal: 15}}>
-                <Text style={{...style.textContacts}}>
-                  Top up from E-Banking
-                </Text>
-                <Text style={{...style.textPhone}}>Rp.300.000</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <View>
-            <TouchableOpacity style={{...style.allContacts}}>
-              <Iconn name="arrow-up" style={{...style.icona}} />
-              <View style={{marginHorizontal: 15}}>
-                <Text style={{...style.textContacts}}>
-                  Transfered to Mamamia
-                </Text>
-                <Text style={{...style.textPhone}}>Rp.50.000</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-
+          {history.length != 0 &&
+            history.map(({id, amount, fullname, type}) => {
+              return (
+                <View key={id}>
+                  <TouchableOpacity style={{...style.allContacts}}>
+                    {type == 'in' ? (
+                      <Iconn name="arrow-down" style={{...style.icon}} />
+                    ) : (
+                      <Iconn name="arrow-up" style={{...style.icona}} />
+                    )}
+                    <View style={{marginHorizontal: 15}}>
+                      <Text style={{...style.textContacts}}>
+                        {type == 'in' ? 'Transfered from' : 'Transfered to'}{' '}
+                        {fullname}
+                      </Text>
+                      <Text style={{...style.textPhone}}>
+                        Rp.{toPrice(amount)}
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              );
+            })}
         </View>
       </ScrollView>
     </View>
